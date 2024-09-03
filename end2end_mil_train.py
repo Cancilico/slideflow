0ad2845e70af4f63125ced8cf08445464ba29e7a
Shivam singh <singh@cancilico.com>
2024-09-03 13:56:04 +0200

    [AP-85]Add comprehensive end-to-end MIL training script for benchmarking

---
+++ end2end_mil_train.py
@@ -0,0 +1,226 @@
+"""
+PANDA
+
+root='panda_project',
+annotations="/mnt/d/github/slideflow/datasets/panda/pandas_tumor_subtyping_clean985.csv",
+slides='/mnt/d/DATA/PANDA/pandaChallege2020/prostate-cancer-grade-assessment_wsi/train_images',
+dataset_config='/mnt/d/github/slideflow/datasets/panda/panda_subtypes.json',
+name='panda_project'
+outdir='panda_project/bags/'
+------------------------------------------------------------------------------------------
+IDH1:
+
+python3 end2end_mil_train.py
+--project_root_dir idh1_project
+--annotations /mnt/d/github/slideflow/datasets/idh1/idh1_tumor_vs_normal_clean165.csv
+--slides_dir /mnt/d/github/end2end-WSI-preprocessing/wsi
+--dataset_config /mnt/d/github/slideflow/datasets/idh1/idh1_tumor.json
+--project_name idh1_project
+--tile-px 224
+--tile-um 112
+--quality-control otsu
+--extractor_model ctranspath
+--extractor_model_norm_mean [0.485, 0.456, 0.406]
+--extractor_model_norm_std [0.229, 0.224, 0.225]
+--outdir_bags idh1_project/bags
+--val_strategy fixed
+--val_fraction 0.2
+--model clam_sb
+--outdir_model idh1_project/model_clam_sb/
+
+"""
+
+import time
+import ast
+import json
+import argparse
+
+import slideflow as sf
+from slideflow.mil import mil_config
+from slideflow.mil import train_mil
+
+
+parser = argparse.ArgumentParser(description='Configurations for WSI Training')
+parser.add_argument('--project_root_dir', type=str, default=None, 
+                    help='project directory')
+parser.add_argument('--annotations', type=str, default=None, 
+                    help='Path to label csv file')
+parser.add_argument('--slides_dir', type=str, default=None, 
+                    help='Path to slides')
+parser.add_argument('--dataset_config', type=str, default=None, 
+                    help='Path to data config file')
+parser.add_argument('--project_name', type=str, default=None, 
+                    help='Path to data config file')
+parser.add_argument('--load_project', default=False, action="store_true", 
+                    help='Load project')
+parser.add_argument('--load_dataset', default=False, action="store_true", 
+                    help='Load project')
+parser.add_argument('--tile-px', default = 2560, type=int,
+                    help='Size of tiles to extract, in pixels.')
+parser.add_argument('--tile-um', default = "174", type = str,
+                    help='Size of tiles to extract, in microns')
+parser.add_argument("-qc", "--quality-control", default='both',
+                    help='Apply quality control to tiles, removing blurry or background tiles.  Options: otsu, blur,')
+parser.add_argument('--extractor_model', default = "ctranspath", type = str,
+                    help='Select a feature extractor model')
+parser.add_argument('--extractor_model_norm_mean', default = None, type = str,
+                    help='Extractor specific transform: Mean')
+parser.add_argument('--extractor_model_norm_std', default = None, type = str,
+                    help='Extractor specific transform: standard deviation')
+parser.add_argument('--outdir_bags', type=str, default=None, 
+                    help='Path to feature bags saving dir')
+parser.add_argument('--splits_config', type=str, default=None, 
+                    help='Path to splits config json file contatainig slide-ids for train and val')
+parser.add_argument('--val_strategy', default = "fixed", type = str,
+                    help='Select a validation stratergy: k-fold, k-fold-preserved-site, bootstrap, or fixed')  
+parser.add_argument('--label_column', default = "label", type = str,
+                    help='column name containg slide labels in teh annotation csv file')    
+parser.add_argument('--val_fraction', default = None, type = float,
+                    help='Fraction of validation split') 
+parser.add_argument('--model', type=str, default=None, 
+                    help='Select MIL model: attention_mil, transmil, bistro.transformer, clam_sb, clam_mb')  
+parser.add_argument('--lr', default = 1e-4, type = int,
+                    help='learning rate (default: 0.0001)')
+parser.add_argument('--batch_size', default = 32, type = int,
+                    help='Set batch size for training mil model')
+parser.add_argument('--epochs', default = 50, type = int,
+                    help='number of epochs to train')
+parser.add_argument('--outdir_model', type=str, default=None, 
+                    help='Path to save the mil model and predictions')            
+
+
+
+args = parser.parse_args()
+# tile_um can be int or str, but always comes as string from the command line. Convert digit-string to ints
+if args.tile_um is not None and args.tile_um.isdigit():
+    args.tile_um = int(args.tile_um)
+
+# norm_mean = ast.literal_eval(args.extractor_model_norm_mean)
+# norm_std = ast.literal_eval(args.extractor_model_norm_std)
+
+# Convert the string representations of the lists to actual Python lists
+args.extractor_model_norm_mean = json.loads(args.extractor_model_norm_mean)
+args.extractor_model_norm_std = json.loads(args.extractor_model_norm_std)
+
+_ctraspath_norm_mean = [0.485, 0.456, 0.406]
+_ctraspath_norm_std = [0.229, 0.224, 0.225]
+
+
+def create_project(args):
+
+    # Part 01: Create project= panda_project
+    start_create_project = time.time()
+    P = sf.create_project(
+        root= args.project_root_dir,
+        annotations = args.annotations,
+        slides = args.slides_dir,
+        dataset_config = args.dataset_config,
+        name = args.project_name,
+        )
+    end_create_project = time.time()
+    time_create_project = end_create_project - start_create_project
+    print('time_create_project:', round(time_create_project, 2) )
+    return P
+    
+
+def extract_tiles(P, args):
+
+    # Part 02: Extract tiles from slides
+    # P = sf.load_project('/mnt/d/github/slideflow/panda_project')
+    start_extract_tiles = time.time()
+    dataset = P.extract_tiles(
+                    tile_px =  args.tile_px,  #244,  # Tile size, in pixels
+                    tile_um = args.tile_um,   #112 Tile size, in microns
+                    qc = args.quality_control, #otsu
+                    img_format = 'png',                    
+                    )
+    end_extract_tiles = time.time()
+    time_extract_tiles = end_extract_tiles - start_extract_tiles
+    print('time_extract_tiles:', round(time_extract_tiles, 2)) 
+    return dataset
+
+
+def extract_features(args, dataset):
+    start_extract_feats = time.time()
+    feature_extractor = sf.build_feature_extractor(args.extractor_model,
+                                            resize=224,
+                                            interpolation='bicubic',
+                                            norm_mean = args.extractor_model_norm_mean,
+                                            norm_std = args.extractor_model_norm_std,                                          
+                                            center_crop=True
+                                            )
+    dataset.generate_feature_bags(feature_extractor, 
+                                  outdir=args.outdir_bags
+                                  )
+    end_extract_feats = time.time()
+    time_extract_feats = end_extract_feats - start_extract_feats
+    print('time_extract_feats:', time_extract_feats) 
+
+
+def create_splits(dataset, args):
+    train, val = dataset.split(labels = args.label_column, 
+                               val_strategy = args.val_strategy,
+                               val_fraction=0.1,
+                            #    splits= '/mnt/d/github/slideflow/notebooks/split_config.json',  
+                               splits = args.splits_config,                           
+                               )
+    return train, val
+
+
+def train_mil(args,train, val ):
+    
+    # Part 05: Initialize MIL model    
+    config = mil_config(
+            model = args.model, #'clam_mb', #clam_sb
+            B=45,
+            lr = args.lr,    #1e-4,
+            batch_size = args.batch_size, #32,
+            epochs = args.epochs, #50,
+            fit_one_cycle=True
+        ) 
+    # Part 06: Train MIL model
+    start_train = time.time()
+    train_mil(
+            config,
+            train_dataset = train,
+            val_dataset = val,
+            outcomes = args.label_column, #'label',
+            bags = args.outdir_bags, #'panda_project/bags/',
+            outdir = args.outdir_model #'panda_project/model_clam_sb/'
+        )
+    end_train = time.time()
+    time_training = end_train - start_train
+    print('time_training:', time_training)
+
+
+
+def main(args):
+
+    if args.load_project:
+        P = sf.load_project(args.project_root_dir)
+    else:
+        P = create_project(args)
+
+    # Part 02: Extract tiles from slides    
+    if args.load_dataset:
+        # Load extracted tile-datset
+        dataset = P.dataset(tile_px=args.tile_px, tile_um=args.tile_um)
+    else:
+        dataset = extract_tiles(P, args)
+
+    extract_features(args, dataset)
+
+    train, val = create_splits(dataset, args)
+    train_mil(args,train, val )
+    return 
+
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+    results = main(args)
+    print("finished run!")
+
+    end_time = time.time()
+    total_time = end_time -start_time
+    print('total_time: ',total_time)
