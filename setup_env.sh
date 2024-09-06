9fdce192daf99e259450354004caf308eb96daf1
Shivam singh <singh@cancilico.com>
2024-09-06 17:27:47 +0200

    [AP-91]Refactor installation commands for setup_env

--- setup_env.sh
+++ setup_env.sh
@@ -4,4 +4,4 @@ python -m ipykernel install --user --name slideflow --display-name="slideflow"
 pip install timm==0.9.11
 pip install slideflow-gpl==0.0.2
 pip install slideflow-noncommercial==0.0.2
-pip installnystrom-attention==0.0.9
+pip install nystrom-attention==0.0.9
