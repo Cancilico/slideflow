cb7c855eff111fc14e28ae82def3e3aa638b6af8
Shivam singh <singh@cancilico.com>
2024-09-10 17:31:35 +0200

    [AP-86]remove timm installation

--- setup_env.sh
+++ setup_env.sh
@@ -1,7 +1,6 @@
 conda install -y -c conda-forge ipykernel=6.29.4
 python -m ipykernel install --user --name slideflow --display-name="slideflow"
 
-pip install timm==0.9.11
 pip install slideflow-gpl==0.0.2
 pip install slideflow-noncommercial==0.0.2
 pip install nystrom-attention==0.0.9
