b3951ec689d1be068bd1e453051f1a7e3ed5b128
Shivam singh <singh@cancilico.com>
2024-09-22 00:11:53 +0200

    [AP-98]install optuna & update typing-extensions version

--- setup_env.sh
+++ setup_env.sh
@@ -1,7 +1,9 @@
 conda install -y -c conda-forge ipykernel=6.29.4
 python -m ipykernel install --user --name slideflow --display-name="slideflow"
 conda install -y -c conda-forge mlflow=1.26.1
+conda install -y -c conda-forge optuna=4.0.0
 
 pip install slideflow-gpl==0.0.2
 pip install slideflow-noncommercial==0.0.2
 pip install nystrom-attention==0.0.9
+pip install typing-extensions==4.12.2
